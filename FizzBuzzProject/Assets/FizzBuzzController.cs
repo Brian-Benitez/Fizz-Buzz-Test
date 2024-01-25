bb38c909737de9c0e2c669922d6c593a05163df1
SalvadoranTrash <107580111+SalvadoranTrash@users.noreply.github.com>
2024-01-24 23:38:46 -0500

    I think im done?
    This code be the worst code ever, somehow.

--- FizzBuzzProject/Assets/FizzBuzzController.cs
+++ FizzBuzzProject/Assets/FizzBuzzController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class FizzBuzzController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //REMAINDER
+    //I FORGOT WHAT HE SAID ABOUT CODE
+    //SHIT.
+    //Encapsate everythiNG
+    [SerializeField]
+    private int AmountOfNums;
+
+    private void Start()
     {
-        
+        FizzBuzzFunction();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FizzBuzzFunction()
     {
-        
+        for (int i = 1; i < AmountOfNums; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                Debug.Log("Fizz Buzz " + i);
+            else if (i % 3 == 0)
+                Debug.Log("fizz " + i);
+            else if (i % 5 == 0)
+                Debug.Log("buzz " + i);
+            else
+                Debug.Log(i);
+        }
     }
 }
